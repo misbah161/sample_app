640ac8da206f58025348605c93a9a08ca61b45d3
Misbah Ahmed <misbah161@live.com>
2013-11-11 15:12:48 +0000

    User model validations

---
+++ db/migrate/20131111150632_add_email_uniqueness_index.rb
@@ -0,0 +1,7 @@
+class AddEmailUniquenessIndex < ActiveRecord::Migration
+  def change
+    add_index :users, :email, :unique => true
+    
+    remove_index :users, :email
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131110114115) do
+ActiveRecord::Schema.define(version: 20131111150632) do
 
   create_table "users", force: true do |t|
     t.string   "name"
