928b34739d62b5cef81f3518ce742c6995f76d41
Misbah Ahmed <misbah161@live.com>
2013-11-15 06:12:41 +0000

    Added password and encrypted password

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131111150632) do
+ActiveRecord::Schema.define(version: 20131115060647) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
   end
 
 end
