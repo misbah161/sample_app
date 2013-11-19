6af56c3b42b7cc8afb0a9bcebf71f984954aa809
Misbah Ahmed <misbah161@live.com>
2013-11-19 06:49:51 +0000

    Working user signin

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -12,7 +12,7 @@ describe "Users" do
 # 	  fill_in "Email",        :with => ""
 # 	  fill_in "Password",     :with => ""
 # 	  fill_in "Confirmation", :with => ""
-# 	  click_button 'signup'
+# 	  click_button 'Sign up'
 # 	  response.should render_template('users/new')
 # 	  response.should have_selector('div#error_explanation')
 # 	end.should_not change(User, :count)
@@ -27,7 +27,7 @@ describe "Users" do
 # 	  fill_in "Email",        :with => "user@example.com"
 # 	  fill_in "Password",     :with => "welcome"
 # 	  fill_in "Confirmation", :with => "welcome"
-# 	  click_button 'signup'
+# 	  click_button 'Sign up'
 # 	  response.should have_selector('div.flash.success', :content => "welcome")
 # 	  response.should render_template('users/show')
 # 	end.should change(User, :count).by(1)
